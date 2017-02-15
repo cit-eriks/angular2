028330dc62b362e8db3ce2411fe0731a6484c886
Erik Schirato <eriks@maccit016858-4.local>
2017-02-15 10:53:46 -0200

    Setup - Quick Start

---
+++ app/contatos/contatos-lista.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'contatos-lista',
+    templateUrl: 'contatos-list.component.html'
+})
+
+export class ContatosListaComponent{}
